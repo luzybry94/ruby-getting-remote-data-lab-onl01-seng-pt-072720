5c4a1181c80ed96a955585413c2365753f7f0def
Chaim Bryski <luzybry94@gmail.com>
2020-09-02 20:41:48 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+  
+  def initialize(url)
+    @url = url
+  end
+  
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+    binding.pry
+  end
+  
+  def parse_json
+    people = JSON.parse(self.get_response_body)
+    people.collect do |person|
+      program["agency"]  
+    end
+  end
+  
+end
